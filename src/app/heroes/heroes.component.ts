69783bc69078a67d1a5a19b786a0b5c3f0f249b0
Jonathan Donadio <jondonadio@gmail.com>
2022-07-28 15:17:42 -0400

    Add click event handler - onSelect method on heroes.component.ts class

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -12,5 +12,9 @@ export class HeroesComponent implements OnInit {
   selectedHero?: Hero;
   heroes = HEROES;
 
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
   ngOnInit(): void {}
 }
