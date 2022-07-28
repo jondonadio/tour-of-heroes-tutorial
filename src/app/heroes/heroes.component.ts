2dbd37ba821c44c4991f10c14e0ec6159a937eaf
Jonathan Donadio <jondonadio@gmail.com>
2022-07-28 16:03:06 -0400

    Import heroService in heroes.component.ts.  Replace heroes property definition with a declaration

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import { HEROES } from 'src/app/mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
@@ -10,7 +10,7 @@ import { HEROES } from 'src/app/mock-heroes';
 export class HeroesComponent implements OnInit {
   constructor() {}
   selectedHero?: Hero;
-  heroes = HEROES;
+  heroes: Hero[] = [];
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
