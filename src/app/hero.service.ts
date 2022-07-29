78efed23654017f256c9d9972b16b55b6d0eee54
jondonadio <jondonadio@gmail.com>
2022-07-29 14:31:48 -0400

    Import HttpClient and HttpHeaders in hero service.  Inject as dependencies.  Create private log method that calls messageService add method

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -3,24 +3,29 @@ import { Observable, of } from 'rxjs';
 import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(private messageService: MessageService) {}
+  constructor(  private http: HttpClient,private messageService: MessageService) {}
 
   getHeroes(): Observable<Hero[]> {
     const heroes = of(HEROES);
-    this.messageService.add('HeroService: fetched heroes');
+    this.log('HeroService: fetched heroes');
     return heroes;
+  }
+
+  private log(message: string) {
+    this.messageService.add(`HeroService: ${message}`);
   }
 
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
     const hero = HEROES.find(h => h.id === id)!;
-    this.messageService.add(`HeroService: fetched hero id=${id}`);
+    this.log(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
 }
