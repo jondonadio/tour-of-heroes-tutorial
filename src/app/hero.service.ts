dc561b452789f2547a249104ff748dd9c629d077
jondonadio <jondonadio@gmail.com>
2022-07-29 14:41:15 -0400

    Add private handleError method to hero service.  Call in getHeroes method with catchError method that was imported

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -4,19 +4,45 @@ import { Hero } from './hero';
 import { HEROES } from './mock-heroes';
 import { MessageService } from './message.service';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { catchError, map, tap } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
 })
 export class HeroService {
-  constructor(  private http: HttpClient,private messageService: MessageService) {}
+  constructor(
+    private http: HttpClient,
+    private messageService: MessageService
+  ) {}
 
-  private heroesUrl = 'api/heroes'
+  private heroesUrl = 'api/heroes';
 
   getHeroes(): Observable<Hero[]> {
-  
     this.log('HeroService: fetched heroes');
-    return this.http.get<Hero[]>(this.heroesUrl)
+    //returns an Observable<Hero[]>
+    return this.http
+      .get<Hero[]>(this.heroesUrl)
+      .pipe(   tap(_ => this.log('fetched heroes')),catchError(this.handleError<Hero[]>('getHeroes', [])));
+  }
+
+  /**
+   * Handle Http operation that failed.
+   * Let the app continue.
+   *
+   * @param operation - name of the operation that failed
+   * @param result - optional value to return as the observable result
+   */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
+      // TODO: send the error to remote logging infrastructure
+      console.error(error); // log to console instead
+
+      // TODO: better job of transforming error for user consumption
+      this.log(`${operation} failed: ${error.message}`);
+
+      // Let the app keep running by returning an empty result.
+      return of(result as T);
+    };
   }
 
   private log(message: string) {
@@ -26,7 +52,7 @@ export class HeroService {
   getHero(id: number): Observable<Hero> {
     // For now, assume that a hero with the specified `id` always exists.
     // Error handling will be added in the next step of the tutorial.
-    const hero = HEROES.find(h => h.id === id)!;
+    const hero = HEROES.find((h) => h.id === id)!;
     this.log(`HeroService: fetched hero id=${id}`);
     return of(hero);
   }
