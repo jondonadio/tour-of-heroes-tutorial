34e05c1699dd799d078e08434c3049c2c82f08b4
jondonadio <jondonadio@gmail.com>
2022-07-29 14:06:08 -0400

    Removed code for heroService in heroes component since no longer used

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
+
 
 @Component({
   selector: 'app-heroes',
@@ -10,16 +10,9 @@ import { MessageService } from '../message.service';
 })
 export class HeroesComponent implements OnInit {
   constructor(
-    private heroService: HeroService,
-    private messageService: MessageService
+    private heroService: HeroService
   ) {}
-  selectedHero?: Hero;
   heroes: Hero[] = [];
-
-  onSelect(hero: Hero): void {
-    this.selectedHero = hero;
-    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
-  }
 
   getHeroes(): void {
     this.heroService.getHeroes().subscribe((heroes) => (this.heroes = heroes));
